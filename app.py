5c8a9900d65aa66659d1fba2a1637ddc36a5c111
Sahil Shaikh <itzmesahilshaikh@gmail.com>
2026-02-05 17:42:59 +0530

    Update Application v.2

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import socket
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from github Action 🚀 App is running on port 8080 on EC2 Instance deployed successful"
+    return render_template(
+        "index.html",
+        host=socket.gethostname(),
+        time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
