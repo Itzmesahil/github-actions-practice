25a48366ad3b99c264379955985999b4192e7672
Sahil Shaikh <itzmesahilshaikh@gmail.com>
2026-02-05 17:36:38 +0530

    update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from github Action 🚀 App is running on port 8080 on EC2 Instance"
+    return "Hello from github Action 🚀 App is running on port 8080 on EC2 Instance deployed successful"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
