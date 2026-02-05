ebd27294f98f69582bca15a532c87e9f3b7cb6b0
Sahil Shaikh <itzmesahilshaikh@gmail.com>
2026-02-05 13:02:30 +0530

    Deploying on EC2

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from Python App 🚀")
-    print("GitHub Actions is running this script successfully!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from github Action 🚀 App is running on port 8080 on EC2 Instance"
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=8080)
